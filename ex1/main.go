2285e2252e6552b091d952c0c10f8cf5c0c6f363
unknown <johannes_zeller@sluz.ch>
2022-10-26 18:12:32 +0200

    solve extra task 1

--- ex1/main.go
+++ ex1/main.go
@@ -1,11 +1,48 @@
 package main
 
-func computeGrade(gotPoints, maxPoints float32) float32 {
-	return gotPoints/maxPoints*5 + 1
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"os"
+)
+
+func computeGrade(gotPoints, maxPoints float32) (float32, error) {
+	if gotPoints > maxPoints {
+		return 0.0, errors.New("The got points can't be over the max points.")
+	}
+	if gotPoints < 0 {
+		return 0.0, errors.New("You can't have negative points.")
+	}
+	return gotPoints/maxPoints*5 + 1, nil
+}
+
+type exam struct {
+	gotPoints float32
+	maxPoints float32
+}
+
+func getRandomExamPoints() float32 {
+	return rand.Float32()*250 - 100
 }
 
 func main() {
+	// main task
 	computeGrade(0, 6)    // 1
 	computeGrade(9.5, 18) // 3.6388888
 	computeGrade(10, 10)  // 6
+
+	// extra task
+	var randomExams []exam
+	for i := 0; i < 20; i++ {
+		randomExams = append(randomExams, exam{getRandomExamPoints(), getRandomExamPoints()})
+	}
+	for _, randomExam := range randomExams {
+		grade, err := computeGrade(randomExam.gotPoints, randomExam.maxPoints)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "compute grade of gotpoints %v, maxpoints %v: %v\n", randomExam.gotPoints, randomExam.maxPoints, err)
+		} else {
+			fmt.Printf("If a student gets %v out of %v points, he or she will receive a %v.\n", randomExam.gotPoints, randomExam.maxPoints, grade)
+		}
+	}
 }
